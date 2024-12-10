f1fbb2aa819bfbb9e7e79666c7b44af3cfd0ed61
Kadam-Suraj <surajkadam381@gmail.com>
2024-12-10 16:54:45 +0530

    add: User and Video model

---
+++ src/models/video.mode.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = Schema(
+    {
+        videoFile: {
+            type: String,
+            required: true
+        },
+        thumbnail: {
+            type: String,
+        },
+        title: {
+            type: String,
+            required: true,
+            min: [2, "Title must have at least 2 characters"]
+        },
+        description: {
+            type: String,
+            min: [8, "Description must have at least 8 characters"]
+        },
+        duration: {
+            type: Number, // from cloudinary
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: false
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    }, { timestamps: true });
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
