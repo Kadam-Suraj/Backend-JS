229342387974f2a716885b1ad708677056f2bdf1
Kadam-Suraj <surajkadam381@gmail.com>
2025-01-01 00:31:10 +0530

    feat: enhance CORS configuration with dynamic origin and method support

--- src/app.js
+++ src/app.js
@@ -9,6 +9,14 @@ app.use(cors({
     credentials: true,
 }));
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN || "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", "true"); // If you use credentials
+    next();
+});
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
