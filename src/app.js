097bf925f47df03328bd04ead323d1b9466b59a2
Kadam-Suraj <surajkadam381@gmail.com>
2025-01-01 11:42:12 +0530

    feat: add custom CORS middleware to set Access-Control-Allow-Origin header

--- src/app.js
+++ src/app.js
@@ -9,13 +9,17 @@ app.use(cors({
     credentials: true,
 }));
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN || "*");
+    next();
+});
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
 // routes import
-// import userRouter from './routes/user.routes.js'
 import userRouter from './routes/user.routes.js'
 import healthcheckRouter from "./routes/healthcheck.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
