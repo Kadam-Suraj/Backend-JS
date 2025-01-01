1a6c0bd89d2bca3f594863bb9d3161b964b1c597
Kadam-Suraj <surajkadam381@gmail.com>
2025-01-01 11:47:33 +0530

    feat: add Access-Control-Allow-Methods header to CORS middleware

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(cors({
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN || "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     next();
