d1770fd0a3d727f013e96d2320b50389787b19c8
Kadam-Suraj <surajkadam381@gmail.com>
2025-01-01 11:57:09 +0530

    feat: simplify CORS middleware by removing redundant headers and using environment variable directly

--- src/app.js
+++ src/app.js
@@ -5,19 +5,11 @@ import cors from "cors"
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "*",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
 }));
 
 console.log("CORS_ORIGIN:", process.env.CORS_ORIGIN);
-
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN || "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    next();
-});
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
